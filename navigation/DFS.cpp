89f609a2649422e8ce61915aa1bc03767a1aaffc
toqqaa <145803764+toqqaa@users.noreply.github.com>
2023-12-11 22:30:44 +0200

    DFS.cpp

--- navigation/DFS.cpp
+++ navigation/DFS.cpp
@@ -4,35 +4,29 @@ using namespace std;
 #define ROW 5
 #define COL 5
 
-// priority (up, right, left, down)
 int dRow[] = {-1, 0, 0, 1};
 int dCol[] = {0, 1, -1, 0};
 
-// if a cell is be visited or not
 bool cellisvisited(bool vis[][COL], int row, int col, int grid[][COL])
 {
-    
     if (row < 0 || col < 0 || row >= ROW || col >= COL)
         return false;
 
-    // If cell is already visited or is an obstacle
     if (vis[row][col] || grid[row][col] == -1)
         return false;
 
-    
     return true;
 }
 
-
 void DFS(int grid[][COL], bool vis[][COL], pair<int, int> start, pair<int, int> goal)
 {
-   
     stack<pair<int, int>> s;
 
-    
     s.push(start);
 
     vis[start.first][start.second] = true;
+
+    vector<int> cellNumbers;
 
     while (!s.empty())
     {
@@ -40,11 +34,18 @@ void DFS(int grid[][COL], bool vis[][COL], pair<int, int> start, pair<int, int> 
         int x = cell.first;
         int y = cell.second;
 
-        cout << grid[x][y] << " ";
+        int cellNumber = x * COL + y + 1;
+        cellNumbers.push_back(cellNumber);
 
         if (x == goal.first && y == goal.second)
         {
             cout << "\nThe Goal is reached\n";
+            cout << "Shortest path: ";
+            for (int i : cellNumbers)
+            {
+                cout << i << " ";
+            }
+            cout << endl;
             return;
         }
 
@@ -75,10 +76,7 @@ int main()
         {-1, 17, 18, 19, -1},
         {-1, 22, -1, -1, 25}};
 
-    // Start point
     pair<int, int> start = {3, 1};
-
-    //  goal point
     pair<int, int> goal = {0, 1};
 
     bool vis[ROW][COL];
@@ -88,3 +86,4 @@ int main()
 
     return 0;
 }
+
